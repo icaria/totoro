252e74c90d611cf5bf27588fb15a6b545d09424b
Stephen Chen <stephen.x.chen@oracle.com>
2015-10-11 20:44:49 -0400

    Pulled thirdparty keys into server side config

--- totoro.py
+++ totoro.py
@@ -7,7 +7,14 @@ app.config.from_object(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    keys = {
+        "mapbox_access_token" : "pk.eyJ1IjoiaWNhcmlhIiwiYSI6ImNpZjczc3I5cTAyb2dyeWx6Z2Mzanlld3cifQ.jBkG4gWBX37zKjrbkX7SRg",
+        "mapbox_map_id": "icaria.cif73spzl02orshlz4eep3nfq",
+        "foursquare_client_id": "IUEZ42RZ5SAY4J0E0TOHB34R15GW2RRV2OQUQNXQIKYONHPN",
+        "foursquare_authentication_url": "https://foursquare.com/",
+        "foursquare_api_url":"https://api.foursquare.com/"
+    }
+    return render_template('index.html', name="Stephen Chen", title="Software Engineer", keys=keys)
 
 
 if __name__ == '__main__':
