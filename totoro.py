bfab1429b1a086d8ae765f2b24aa2bfa88762881
Stephen Chen <stephenchen0@gmail.com>
2022-12-22 12:03:50 -0800

    Update totoro.py

--- totoro.py
+++ totoro.py
@@ -12,23 +12,22 @@ mapbox_access_token = os.environ.get('mapbox_access_token')
 
 @app.route('/')
 def index():
-    if foursquare_access_token is not None:
-        foursquare_client = foursquare.Foursquare(client_id=foursquare_client_id,
-                                                  client_secret=foursquare_client_secret)
+    foursquare_client = foursquare.Foursquare(client_id=foursquare_client_id,
+                                              client_secret=foursquare_client_secret)
 
-        last_check_in = foursquare_client.users.checkins(params={'limit': 1})['checkins']['items'][0]
-        venue = last_check_in['venue']
-        latitude = venue['location']['lat']
-        longitude = venue['location']['lng']
+    last_check_in = foursquare_client.users.checkins(params={'limit': 1})['checkins']['items'][0]
+    venue = last_check_in['venue']
+    latitude = venue['location']['lat']
+    longitude = venue['location']['lng']
 
-        keys = {
-            "mapbox_access_token" : mapbox_access_token,
-            "venue": venue['name'],
-            "lat": latitude,
-            "lng": longitude
-        }
+    keys = {
+        "mapbox_access_token" : mapbox_access_token,
+        "venue": venue['name'],
+        "lat": latitude,
+        "lng": longitude
+    }
 
-        return render_template('index.html', name="Stephen Chen", title="Software Development Engineer", keys=keys)
+    return render_template('index.html', name="Stephen Chen", title="Software Development Engineer", keys=keys)
 
 
 if __name__ == '__main__':
